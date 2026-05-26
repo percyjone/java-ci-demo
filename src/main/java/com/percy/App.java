7f4a0ae6d9d108a90b0362fb88269dc8121d56a1
percyjone <percyjone1234@gmail.com>
2026-05-26 19:37:00 +0530

    added workflow

--- src/main/java/com/percy/App.java
+++ src/main/java/com/percy/App.java
@@ -5,6 +5,6 @@ package com.percy;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to GitHub Actions CI/CD");
     }
 }
